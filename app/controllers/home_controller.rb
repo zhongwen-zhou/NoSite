72393016f0c136a140a159d664292724dfa7a1d5
tony <zhongwenzhou.wolf@gmail.com>
2014-03-13 14:48:07 +0800

    修改首页宣战，提供一个思路

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 class HomeController < ApplicationController
   def index
-    @articles = Article.desc(:created_at).limit(5)
+    @articles = Article.desc(:created_at).limit(3)
     @leagues = League::League.all.limit(6)
     @share_content = {:web_spread => true}
     @guess_groups = [['中国队', '日本队'], ['皇马', '巴萨']]
