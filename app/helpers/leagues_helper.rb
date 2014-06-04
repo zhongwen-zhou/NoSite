01e0c82611f8c17656490e0f2e38a936ff3f5656
zhongwen <zhongwenzhou.wolf@gmail.com>
2014-06-04 20:58:25 +0800

    工会聊天

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -6,7 +6,8 @@ module LeaguesHelper
 
 	def can_publish_message?(league)
 		return false unless league.present?
-		member = league.members.find(current_user.id)
+		member = league.members.where(:user_id => current_user.id).first
+		return false if member.nil?
 		return true if member.is_admin? || member.role == 2
 		return false
 	end
