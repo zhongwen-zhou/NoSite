d5790cd2035b29e16a7572ed060437cc803aa5af
zhongwen <zhongwenzhou.wolf@gmail.com>
2015-03-12 20:52:04 +0800

    站内信

--- app/controllers/message/home_controller.rb
+++ app/controllers/message/home_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Message::HomeController < ApplicationController
   def index
-  	@communications = Message::Communication.user_communication(current_user)
+  	@communications = Message::Communication.user_communication(current_user).paginate(:page => params[:page], :per_page => 10)
   end
 end
--- app/controllers/message/sys_messages_controller.rb
+++ app/controllers/message/sys_messages_controller.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 class Message::SysMessagesController < ApplicationController
   def index
-  	@messages = Message::SysMessage.where(:receiver => current_user)#.find(params[:id]).messages.paginate(:page => params[:page], :per_page => 10)
+  	# @messages = Message::SysMessage.where(:receiver => current_user)#.find(params[:id]).messages.paginate(:page => params[:page], :per_page => 10)
+  	@messages = Message::SysMessage.where(:receiver => current_user).paginate(:page => params[:page], :per_page => 10)
   end
 end
